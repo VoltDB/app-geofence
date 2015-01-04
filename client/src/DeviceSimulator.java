69892a03968ea728b079042cfaa4579dda970a56
John Hugg <jhugg@voltdb.com>
2015-01-04 18:04:18 -0500

    Update copyright for 2015.

--- client/src/DeviceSimulator.java
+++ client/src/DeviceSimulator.java
@@ -1,3 +1,26 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2015 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package client;
 
 import java.io.*;
@@ -39,14 +62,14 @@ public class DeviceSimulator {
 	    while ((line = br.readLine()) != null) {
                 i++;
                 String[] record = line.split("\\t"); // split by tab
-                
+
                 // add to zipcodePositions set
                 String zipcode = record[0];
                 Position p = new Position();
                 p.latitude = Double.parseDouble(record[7]);
                 p.longitude = Double.parseDouble(record[8]);
                 zipcodePositions.put(zipcode,p);
-                
+
 		if (i % 10000 == 0)
 		    System.out.println("  read " + i + " lines");
 	    }
@@ -78,7 +101,7 @@ public class DeviceSimulator {
             p.latitude = zipPos.latitude;
             p.longitude = zipPos.longitude;
             devicePositions[i] = p;   // save the position of the device for later
-            
+
             // insert device
             client.callProcedure(new BenchmarkCallback("DEVICES.insert"),
                                  "DEVICES.insert",
@@ -99,7 +122,7 @@ public class DeviceSimulator {
     }
 
     public void randomMovement(Client client) throws Exception {
-        
+
         // get a random device
         int d = rand.nextInt(devicePositions.length);
 
--- client/src/GeoFenceBenchmark.java
+++ client/src/GeoFenceBenchmark.java
@@ -1,3 +1,26 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2015 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package client;
 
 import java.io.*;
@@ -13,7 +36,7 @@ public class GeoFenceBenchmark extends BaseBenchmark {
     // constructor
     public GeoFenceBenchmark(BenchmarkConfig config) {
         super(config);
-        
+
         this.deviceCount = config.devices;
     }
 
@@ -37,7 +60,7 @@ public class GeoFenceBenchmark extends BaseBenchmark {
 
     public static void main(String[] args) throws Exception {
         BenchmarkConfig config = BenchmarkConfig.getConfig("GeoFenceBenchmark",args);
-        
+
         BaseBenchmark c = new GeoFenceBenchmark(config);
         c.runBenchmark();
     }
